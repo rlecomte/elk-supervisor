2bfb7660236af5ff301d01f0866199354743acc3
Romain Lecomte <romain.lecomte.pub@gmail.com>
2015-04-18 16:31:32 +0200

    feat(java app): add java example

---
+++ java-app-example/README.md
@@ -0,0 +1,1 @@
+Very simple app to present a way to log info and metric from a java application
---
+++ java-app-example/src/main/java/orl/rl/monitoring/App.java
@@ -0,0 +1,25 @@
+package orl.rl.monitoring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App
+{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    private static final Logger MONITORING_LOGGER = LoggerFactory.getLogger("org.rl.monitoring");
+
+    public static void main( String[] args )
+    {
+        int metricInt = 0;
+        String metricStr = "Hello";
+
+        LOGGER.info("Hello World!");
+
+        /*
+            It's important to set '-int' suffix for integer metric because elasticsearch mapping
+            is configure to check this suffix in order to consider metric like integer.
+        */
+        MONITORING_LOGGER.info("metric-int={}, metric-str={}", ++metricInt, metricStr);
+    }
+}
